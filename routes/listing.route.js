ccbd94ebb5002543c74d845422726dabedea7a4d
801DevLabs <801DevLabs@gmail.com>
2019-02-20 15:26:37 -0700

    updated

--- routes/listing.route.js
+++ routes/listing.route.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const listing_controller = require('../controllers/listing.controller')
 
+router.get('/test', listing_controller.test)
 router.get('/', listing_controller.view_all)
 router.get('/city/:city', listing_controller.listing_city)
 router.get('/state/:state', listing_controller.listing_state)
